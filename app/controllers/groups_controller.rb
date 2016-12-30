274931f47dffcf7c8e86b656809d8c1f5a098fe4
cheng df <cheng.010021@gmail.com>
2016-12-30 16:46:24 +0800

    use before-action to find_group_and _check_out_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_checkout_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -10,7 +10,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+
   end
 
   def new
@@ -29,7 +29,7 @@ class GroupsController < ApplicationController
 
 
   def update
-    @group = Group.find(params[:id])
+
     if @group.update(group_params)
     redirect_to groups_path, notice: "Update Success"
   else
@@ -38,7 +38,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
+
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
@@ -47,5 +47,13 @@ class GroupsController < ApplicationController
 
   def group_params
     params.require(:group).permit(:title, :description)
+  end
+
+  def find_group_and_checkout_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
   end
 end
